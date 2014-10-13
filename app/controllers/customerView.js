57888390de53ba64b17d9a0e60e4b21a8f902c12
Ryan Sunaryo <rsunaryo@evotech.co.id>
2014-10-13 14:33:53 +0700

    change default email

--- app/controllers/customerView.js
+++ app/controllers/customerView.js
@@ -5,6 +5,6 @@ $.companynameLabel.text = args.get('companyName') || 'Default Company Name';
 $.firstnameLabel.text = args.get('firstName') || 'Default First Name';
 $.positionLabel.text = args.get('position') || 'Default Position';
 $.phoneLabel.text = args.get('phone') || 'Default Phone';
-$.emailLabel.text = args.get('email') || 'Default Email';
+$.emailLabel.text = args.get('email') || 'Default Email COST';
 
 //TESTTTT DARI REYNALD
