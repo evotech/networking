8e7fa3e139157e6cf703f4d3d4ceae1ba6443d9c
Ryan Sunaryo <rsunaryo@evotech.co.id>
2014-10-13 14:40:24 +0700

    add last name label

--- app/controllers/customerView.js
+++ app/controllers/customerView.js
@@ -4,7 +4,8 @@ $.IDLabel.text = args.get('id') || 'Default ID';
 $.companynameLabel.text = args.get('companyName') || 'Default Company Name';
 $.firstnameLabel.text = args.get('firstName') || 'Default First Name';
 $.positionLabel.text = args.get('position') || 'Default Position';
+$.lastnameLabel.text = args.get('lastName') || 'Default Last Name';
 $.phoneLabel.text = args.get('phone') || 'Default Phone';
-$.emailLabel.text = args.get('email') || 'Default Email COST';
+$.emailLabel.text = args.get('email') || 'Default Email';
 
 //TESTTTT DARI REYNALD
